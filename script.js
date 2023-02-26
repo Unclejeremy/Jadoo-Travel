f027545c1a816c12ec05cad052307b03ab938959
Unclejeremy <akpeghaghaoghenevwede@gmail.com>
2023-02-26 17:11:54 +0100

    add new js file

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Jerry");
+
+
+
+let box = "Jerry";
+// alert(box);
+
+box = "Cletus";
+alert(box);
+
+const pie = 3.142;
+pie = 23;
+
+
+// go to the market with N5,000
+let amount = 5000;
+// but items
+let car = 200;
+let balance = amount - car;
+alert(balance);
+
+
+
+String
+integer
+float 
+// boolean (true or false)
+undefined
+null
+
+
+arrays = ["jerry", "emma", 3, false]
+Object = {}
+
+functions 
